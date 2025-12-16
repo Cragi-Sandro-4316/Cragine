bfe98710fb4b5d244ec06804aeed450e7901b143
Cragi-Sandro4316 <filippomacciardi4@gmail.com>
2025-12-16 22:50:48 +0100

    Fixed vkb dependency issues

--- Project/src/main.cpp
+++ Project/src/main.cpp
@@ -1,7 +1,9 @@
+#include "InputModule/InputModule.h"
 #include <Cragine.h>
 
 int main() {
     crg::App app{};
+    app.addModule(crg::InputModule{});
     app.run();
     LOG_TRACE("Log from game project");
     return 0;
