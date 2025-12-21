d49d7636fa303f446ed3a9294a903e13d71b40c0
Cragi-Sandro4316 <filippomacciardi4@gmail.com>
2025-12-21 17:19:15 +0100

    vulkan device selection and small renderer restructure

--- Cragine/include/Renderer/Renderer.cpp
+++ Cragine/include/Renderer/Renderer.cpp
@@ -1,15 +1,19 @@
 #include "Renderer.h"
-#include "Renderer/Instance/Instance.h"
+#include "VkBootstrap.h"
 #include "utils/Logger.h"
+#include "vulkan/vulkan.hpp"
+#include <GLFW/glfw3.h>
+#include <vulkan/vulkan_core.h>
 
 
-namespace crg {
+namespace crg::renderer {
 
     Renderer::Renderer(GLFWwindow* window) :
     m_window(window) {
         LOG_CORE_INFO("Initialising renderer");
 
-        m_instance = makeInstance("Cragine", m_deletionQueue);
+        makeInstance("Cragine");
+        selectDevice();
     }
 
     Renderer::~Renderer() {
@@ -18,6 +22,57 @@ namespace crg {
             m_deletionQueue.back()(m_instance);
             m_deletionQueue.pop_back();
         }
+    }
+
+    void Renderer::makeInstance(const char* appName) {
+        LOG_CORE_INFO("Creating vkInstance");
+
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        vkb::InstanceBuilder builder;
+
+        auto ret = builder
+            .set_app_name(appName)
+            .request_validation_layers(true)
+            .enable_extensions(glfwExtensionCount, glfwExtensions)
+            .use_default_debug_messenger()
+            .require_api_version(1, 3, 0)
+        .build();
+
+        m_vkbInstance = ret.value();
+
+        m_instance = m_vkbInstance.instance;
+        m_debugMessenger = m_vkbInstance.debug_messenger;
+
+    }
+
+    void Renderer::selectDevice() {
+        LOG_CORE_INFO("Selecting physical device");
+        auto err = glfwCreateWindowSurface(m_instance, m_window, nullptr, (VkSurfaceKHR*)(&m_surface));
+
+        VkPhysicalDeviceVulkan13Features features { VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_3_FEATURES };
+        features.dynamicRendering = true;
+        features.synchronization2 = true;
+
+        VkPhysicalDeviceVulkan12Features features12{ VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_2_FEATURES };
+        features12.bufferDeviceAddress = true;
+        features12.descriptorIndexing = true;
+
+        vkb::PhysicalDeviceSelector selector {m_vkbInstance};
+        m_vkbPhysicalDevice = selector
+            .set_minimum_version(1, 3)
+            .set_required_features_13(features)
+            .set_required_features_12(features12)
+            .set_surface(m_surface)
+            .select()
+        .value();
+
+        vkb::DeviceBuilder deviceBuilder {m_vkbPhysicalDevice};
+
+        m_vkbDevice = deviceBuilder.build().value();
+        m_device = m_vkbDevice.device;
+        m_physicalDevice = m_vkbPhysicalDevice.physical_device;
     }
 
 }
--- Cragine/include/Renderer/Renderer.h
+++ Cragine/include/Renderer/Renderer.h
@@ -1,12 +1,16 @@
 #pragma once
 
+#include "VkBootstrap.h"
+#include "vulkan/vulkan.hpp"
 #include <deque>
 #include <functional>
+#include <vulkan/vulkan_core.h>
 #define VUILKAN_HPP_NO_EXCEPTIONS
 #include <vulkan/vulkan.hpp>
+#define GLFW_INCLUDE_VULKAN
 #include <GLFW/glfw3.h>
 
-namespace crg {
+namespace crg::renderer {
 
     class Renderer {
     public:
@@ -17,6 +21,13 @@ namespace crg {
         Renderer(const Renderer&) = delete;
         Renderer operator=(Renderer&) = delete;
 
+
+
+    private:
+        void makeInstance(const char* appName);
+
+        void selectDevice();
+
     private:
 
         // @brief Destruction queue
@@ -24,10 +35,22 @@ namespace crg {
 
         vk::Instance m_instance = VK_NULL_HANDLE;
 
+        vk::DebugUtilsMessengerEXT m_debugMessenger = VK_NULL_HANDLE;
+
+        vk::SurfaceKHR m_surface = VK_NULL_HANDLE;
+
+        vk::PhysicalDevice m_physicalDevice = VK_NULL_HANDLE;
+
+        vk::Device m_device = VK_NULL_HANDLE;
+
+
+
         GLFWwindow* m_window;
 
 
-
+        vkb::Instance m_vkbInstance;
+        vkb::PhysicalDevice m_vkbPhysicalDevice;
+        vkb::Device m_vkbDevice;
     };
 
 }
